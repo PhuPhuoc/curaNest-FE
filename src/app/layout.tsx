d57bd72661d71afb3e9ba85e418bdcafe2708b52
RedQW <danhtang226@gmail.com>
2024-08-31 15:25:37 +0700

    fix import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Providers } from "@/app/provider";
+import { Providers } from "@/app/providers";
 import { ToastContainer } from "react-toastify";
 
 const inter = Inter({ subsets: ["latin"] });
