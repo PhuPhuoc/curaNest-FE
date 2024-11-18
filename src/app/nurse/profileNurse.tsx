c191a7447fbbbbcc9a417a14406191055b950775
nhantam2502 <nhantamhd@gmail.com>
2024-11-18 13:00:21 +0700

    Fetch API for Nurse Profile

--- src/app/nurse/profileNurse.tsx
+++ src/app/nurse/profileNurse.tsx
@@ -1,10 +1,14 @@
 // components/ProfileDrawer.tsx
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Avatar, Col, Divider, Drawer, List, Row } from "antd";
 import Link from "next/link";
 import { Button, Chip } from "@nextui-org/react";
 import { useAppContext } from "@/app/app-provider";
 import { useRouter } from "next/navigation";
+import techniqueApiRequest from "@/apiRequests/technique/technique";
+import { Technique } from "@/types/technique";
+import { DetailNurse } from "@/types/nurse";
+import nurseApiRequest from "@/apiRequests/nurse/nurse";
 
 interface DescriptionItemProps {
   title: string;
@@ -45,11 +49,47 @@ const services = [
   },
 ];
 
+const colors = ["bg-blue-500", "bg-green-500", "bg-red-500", "bg-yellow-500"];
+
 const ProfileNurse: React.FC<ProfileDrawerProps> = ({ open, onClose }) => {
-  const { setUser } = useAppContext();
+  const { setUser, setAccount, user } = useAppContext();
   const router = useRouter();
+  const [techniques, setTechniques] = useState<Technique[]>([]);
+  const [nurseList, setNurseList] = useState<DetailNurse>();
+
+  useEffect(() => {
+    if (user?.id) {
+      const fetchDetailNurse = async () => {
+        try {
+          const response = await nurseApiRequest.detailNurse(user?.id, "admin");
+          setNurseList(response.payload.data);
+        } catch (error) {
+          console.error("Failed to fetch nurses:", error);
+        }
+      };
+      fetchDetailNurse();
+    }
+  }, [user]);
+  console.log("điều dưỡng: ", nurseList);
+  useEffect(() => {
+    const fetchTechniques = async () => {
+      try {
+        const response = await techniqueApiRequest.getTechnique();
+        const techniquesData = Array.isArray(response.payload.data)
+          ? response.payload.data
+          : [response.payload.data];
+        setTechniques(techniquesData);
+      } catch (error) {
+        console.error("Failed to fetch techniques:", error);
+      }
+    };
+
+    fetchTechniques();
+  }, []);
+
   function handleLogout() {
     setUser(null);
+    setAccount(null);
     localStorage.removeItem("user");
     document.cookie =
       "userRole=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
@@ -65,35 +105,33 @@ const ProfileNurse: React.FC<ProfileDrawerProps> = ({ open, onClose }) => {
         onClose={onClose}
         open={open}
       >
-        {/* <p className="text-xl font-bold mb-4">Thông tin điều dưỡng</p> */}
-
         {/* Centered Avatar */}
         <div className="flex justify-center mb-2">
-          <Avatar
-            src="https://i.pravatar.cc/150?u=a04258114e29026708c"
-            className="w-28 h-28"
-          />
+          <Avatar src={nurseList?.avatar} className="w-28 h-28" />
         </div>
 
         <div className="flex justify-center mb-8">
-          <p className="text-xl font-bold">Nguyễn Thị A</p>
+          <p className="text-xl font-bold">{nurseList?.full_name}</p>
         </div>
 
         <Row>
           <Col span={12}>
-            <DescriptionItem title="Số điện thoại" content="0974832634" />
+            <DescriptionItem
+              title="Số điện thoại"
+              content={nurseList?.phone_number}
+            />
           </Col>
           <Col span={12}>
-            <DescriptionItem
-              title="Tài khoản"
-              content="nguyenthia@example.com"
-            />
+            <DescriptionItem title="Tài khoản" content={nurseList?.email} />
           </Col>
         </Row>
 
         <Row>
           <Col span={12}>
-            <DescriptionItem title="Căn cước công dân" content="079303000288" />
+            <DescriptionItem
+              title="Căn cước công dân"
+              content={nurseList?.citizen_id}
+            />
           </Col>
           <Col span={12}>
             <DescriptionItem title="Ngày sinh" content="25/02/1990" />
@@ -103,8 +141,17 @@ const ProfileNurse: React.FC<ProfileDrawerProps> = ({ open, onClose }) => {
         <Row>
           <Col span={24}>
             <DescriptionItem
+              title="Nơi làm việc hiện tại"
+              content={nurseList?.current_workplace}
+            />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col span={24}>
+            <DescriptionItem
               title="Châm ngôn sống"
-              content="Tận tâm và chuyên nghiệp trong việc chăm sóc bệnh nhân, luôn đặt lợi ích của bệnh nhân lên hàng đầu."
+              content={nurseList?.slogan}
             />
           </Col>
         </Row>
@@ -114,11 +161,21 @@ const ProfileNurse: React.FC<ProfileDrawerProps> = ({ open, onClose }) => {
         <p className="site-description-item-profile-p font-bold text-lg mb-4">
           Trình độ làm việc
         </p>
+
+        <Row>
+          <Col span={24}>
+            <DescriptionItem
+              title="Chuyên ngành"
+              content={nurseList?.expertise}
+            />
+          </Col>
+        </Row>
+
         <Row>
           <Col span={24}>
             <DescriptionItem
               title="Học vấn"
-              content="Cử nhân Điều dưỡng - Đại học Y Hà Nội"
+              content={nurseList?.education_level}
             />
           </Col>
         </Row>
@@ -127,7 +184,7 @@ const ProfileNurse: React.FC<ProfileDrawerProps> = ({ open, onClose }) => {
           <Col span={24}>
             <DescriptionItem
               title="Kinh nghiệm làm việc"
-              content="5 năm kinh nghiệm tại Bệnh viện Bạch Mai"
+              content={nurseList?.work_experience}
             />
           </Col>
         </Row>
@@ -136,7 +193,7 @@ const ProfileNurse: React.FC<ProfileDrawerProps> = ({ open, onClose }) => {
           <Col span={24}>
             <DescriptionItem
               title="Chứng chỉ"
-              content="Điều dưỡng viên (2018), Sơ cấp cứu (2018)"
+              content={nurseList?.certificate}
             />
           </Col>
         </Row>
@@ -146,12 +203,14 @@ const ProfileNurse: React.FC<ProfileDrawerProps> = ({ open, onClose }) => {
             title="Kỹ năng đăng ký làm việc ở curanest"
             content=""
           />
-          {services.map((service, index) => (
+          {techniques.map(({ id, name }, index) => (
             <Chip
-              key={index}
-              className={`${service.color} ${service.textColor}`}
+              key={id}
+              className={`text-white font-bold ${
+                colors[index % colors.length]
+              } px-4 py-2`}
             >
-              {service.name}
+              {name}
             </Chip>
           ))}
         </div>
