b1d06b7db3f701843cdb8eae675ead9e0acdd216
RedQW <danhtang226@gmail.com>
2024-11-28 00:10:48 +0700

    fix missing hour in schedule

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://api.curanest.com.vn/api/v1/:path*", 
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
